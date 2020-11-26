82251cb489aa0f4f3d6e817d91229fe4444d33fd
Natcha Pradappet <bignacho@gmail.com>
2020-11-26 12:45:29 -0600

    Navbar now responsive, added subtitle to homepage, removed unnecessary files

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,50 @@
 <template>
   <div id="app">
     <v-app>
-      <AppBar />
-      <BackgroundVideo />
+      <!--      <app-bar />-->
+      <navbar />
+
+      <video
+        playsinline
+        autoplay
+        muted
+        loop
+        poster
+        style="object-fit: cover; width: 100vw;height: 100vh;position: fixed;top: 0;left: 0; padding: 0"
+      >
+        <source
+          src="../public/img/nebula.mp4"
+          type="video/mp4"
+        >
+      </video>
       <router-view />
     </v-app>
   </div>
 </template>
 
 <script>
-export default {
-  name: "App",
-  components: {
-    BackgroundVideo: () => import("@/components/home/BackgroundVideo"),
-    Toolbar: () => import("@/components/Toolbar"),
-    Toolbar3: () => import("@/components/Toolbar3"),
-    // Toolbar2: () => import('@/components/Toolbar2'),
-    AppBar: () => import("@/components/AppBar")
-  },
-  data: () => ({
-    date: new Date().getFullYear()
-  })
-};
+  export default {
+    name: 'App',
+    components: {
+      Navbar: () => import('../src/components/Navbar')
+    }
+  }
 </script>
 
 <style lang="sass">
 @import url("https://use.typekit.net/ggq3gsy.css")
 
+*
+  cursor: url(../public/img/rocketship.png), auto
+
+video
+  object-fit: cover
+  width: 100vw
+  height: 100vh
+  position: fixed
+  top: 0
+  left: 0
+
 .tinted-bg
   background: rgba(0, 0, 0, .66)
-
-  
-body,
-span,
-div
-  font-family: monotalic, sans-serif
-  cursor: url('/img/rocketship.png'), auto
-
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <v-container fluid fill-height class="pa-0">
-    <CenterImage />
-    <!-- <Button /> -->
-  </v-container>
+  <v-content>
+    <v-container
+      fluid
+      fill-height
+    >
+      <v-col>
+        <v-row>
+          <v-img
+            src="/img/planet-nacho-splash.png"
+            contain
+            min-height="35vh"
+            max-height="50vh"
+          />
+        </v-row>
+
+        <v-row
+          justify="center"
+          class="warning--text landingPageSubtitle"
+        >
+          A PERSONAL WEBSITE
+        </v-row>
+      </v-col>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
-export default {
-  name: "App",
-  components: {
-    CenterImage: () => import("@/components/home/CenterImage"),
-    Button: () => import("@/components/home/Button")
+  export default {
+    name: 'App',
+    components: {
+    }
   }
-};
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+
+.landingPageSubtitle
+  font-size: medium
+  font-family: monotalic, monospace
+  letter-spacing: .3em
+  font-weight: 600
+</style>
