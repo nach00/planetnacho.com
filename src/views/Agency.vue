51d8bb031234ac254942bef87dc5385b9fbce9c4
Natcha Pradappet <bignacho@gmail.com>
2021-08-26 03:19:38 -0500

    Update Agency.vue

--- src/views/Agency.vue
+++ src/views/Agency.vue
@@ -16,7 +16,7 @@
 
       <v-container
         id="info"
-        class="mt-5"
+        class="mt-5 d-none d-md-flex"
       >
         <v-row>
           <v-col>
@@ -62,7 +62,53 @@
           </v-col>
         </v-row>
       </v-container>
-
+      <v-container
+        id="info"
+        class="mt-5 d-md-none"
+        fluid
+      >
+        <v-col>
+          <div class="cheese--text overline fancy-link">
+            Current Availability
+          </div>
+          <div class="orange-divider mb-5" />
+          <div style="display:flex; justify-content: space-between">
+            <div class="font-weight-black mb-5">
+              Not accepting
+            </div>
+            <v-btn
+              outlined
+              color="white"
+              small
+              class="font-weight-black"
+              href="https://v2.waitwhile.com/welcome/planetnacho"
+            >
+              Join the waitlist
+            </v-btn>
+          </div>
+          <div class="status body-2 mb-5">
+            We usually work on multiple projects at once, but we are not currently accepting new clients at the moment. Join the waitlist to get notified when we're ready to take on more work!
+          </div>
+        </v-col>
+        <v-col>
+          <div class="cheese--text overline fancy-link">
+            Closed Alpha Testing Status
+          </div>
+          <div class="orange-divider mb-5" />
+          <div class="body-2">
+            Alpha testing is now live! Be the first to help shape the future of our design process. Used to collect initial market research, generate product strategy, and gather client feedback.
+          </div>
+        </v-col>
+        <v-col>
+          <div class="cheese--text overline fancy-link">
+            Pro Bono Services
+          </div>
+          <div class="orange-divider mb-5" />
+          <div class="body-2">
+            We're happy to support your 501c3/Non-Profit organization! Animal/pet rescue organizations prioritized.
+          </div>
+        </v-col>
+      </v-container>
       <v-container
         id="content"
         class="mt-10"
@@ -73,7 +119,7 @@
 
         <div class="orange-divider mb-5" />
 
-        <v-container>
+        <v-container class="d-none d-md-flex">
           <v-row style="border: white 2px solid; background: none">
             <v-col
               id="left"
@@ -181,7 +227,116 @@
             </v-col>
           </v-row>
         </v-container>
+
+        <v-container class="d-md-none">
+          <v-row style="border: white 2px solid; background: none">
+            <v-col
+              id="left"
+              class=" d-flex flex-column"
+            >
+              <v-container class="text-uppercase">
+                <div class="font-weight-black mb-5">
+                  ON-DEMAND UX/UI MOCKUP SCREENS
+                </div>
+                <v-container>
+                  <v-row class="justify-space-between align-center">
+                    <v-col class="cheese--text text-uppercase text-center darken-4 grey">
+                      <v-icon
+                        class="cheese--text mr-1"
+                      >
+                        mdi-cellphone
+                      </v-icon>
+                      5 Mobile screens
+                    </v-col>
+                    <v-col class="or text-uppercase font-weight-black text-center">
+                      - or -
+                    </v-col>
+                    <v-col class="cheese--text text-uppercase text-center darken-4 grey">
+                      <v-icon
+                        class="cheese--text mr-1"
+                      >
+                        mdi-monitor
+                      </v-icon>
+                      3 desktop screens
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-container>
+
+              <v-container class="">
+                Custom professional mockup screens for mobile, tablet, or desktop devices that are ready to handoff to developers or for use in shareholder presentations.</span>
+              </v-container>
+
+              <v-container class="">
+                <span class="text-uppercase font-weight-black">Deliverables</span>
+                <ul class="body-2">
+                  <li>Finalized high-fidelity mockup mobile/tablet screens</li>
+                  <li>Original Adobe XD or Figma file</li>
+                  <li>All associated assets (icons, logos, fonts, if applicable)</li>
+                  <li>Developer handoff export link for style guide</li>
+                  <li>Multiple options, alternatives, and wireframes worked out</li>
+                  <li>Includes information architecture, user flows, and basic research</li>
+                  <li>Clickthrough prototype and daily updates available upon request</li>
+                </ul>
+              </v-container>
+            </v-col>
+
+            <v-container class="">
+              <v-container
+                class="d-flex fluid py-0"
+              >
+                <v-icon
+                  class="white--text mr-5"
+                  small
+                >
+                  mdi-clock
+                </v-icon>
+                2 week delivery
+              </v-container>
+              <v-container class="d-flex fluid py-0 ">
+                <v-icon
+                  class="white--text mr-5"
+                  small
+                >
+                  mdi-calendar
+                </v-icon>
+                3 project meetings
+              </v-container>
+              <v-container class="d-flex fluid py-0">
+                <v-icon
+                  class="white--text mr-5"
+                  small
+                >
+                  mdi-pen
+                </v-icon>
+                3 revisions
+              </v-container>
+            </v-container>
+            <v-container class="d-flex">
+              <v-container
+                class="d-flex"
+              >
+                <span
+                  class="text-uppercase font-weight-black"
+                >
+                  Starting a
+                </span>
+                <span class="price cheese--text ml-1 font-weight-black">
+                  $3K
+                </span>
+              </v-container>
+              <v-btn
+                align="end"
+                class=" cheese font-weight-black"
+                href="https://calendly.com/npradappet/jam"
+              >
+                Let's jam!
+              </v-btn>
+            </v-container>
+          </v-row>
+        </v-container>
       </v-container>
+    </v-container>
     </v-container>
   </v-content>
 </template>
